a5dfabea46e29f5836af78d0ef6188fd6b61c8f1
jessiehxw <hexiawei1999@outlook.com>
2023-07-16 21:32:52 +0800

    Create Product Page completed.

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,6 +1,36 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link, useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        width: "133px",
+        height: "40px",
+        border: "none",
+        "border-radius": "5px",
+        textTransform: "none",
+        margin: theme.spacing(1),
+        backgroundColor: "#5048E5",
+        color: "#FFFFFF",
+        "&:hover": {
+            backgroundColor: "#512da8",
+        },
+    },
+}));
 
 export default function Home() {
-    return <div></div>;
+    const classes = useStyles();
+
+    return (
+        <div>
+            <h1>Products</h1>
+            {/* Filter */}
+            <Link to="/create">
+                <button className={classes.button}>Add Product</button>
+            </Link>
+            {/* Product List */}
+            {/* Page Nav */}
+        </div>
+    );
 }
