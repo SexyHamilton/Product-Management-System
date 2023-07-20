cc0d5b7f15c45c8c72a0d1eac63fa0e97c061cac
Sexy Hamilton <wanghanyu329@gmail.com>
2023-07-20 00:30:13 -0400

    new try

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,24 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./screens/Home";
-import Signin from "./screens/Signin";
-import Signup from "./screens/Signup";
-import Update from "./screens/Update";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import CreateProduct from "./screens/CreateProduct";
-import ErrorPage from "./screens/ErrorPage";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <header>
-          <Header />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Signin />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/update" element={<Update />}></Route>
-            <Route path="/create" element={<CreateProduct />}></Route>
-            <Route path="/*" element={<ErrorPage />} />
-          </Routes>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
