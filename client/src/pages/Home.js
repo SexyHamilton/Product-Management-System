233e6e0feddad3a4c4d4b22edc23659ea6dee34a
jessiehxw <hexiawei1999@outlook.com>
2023-07-29 02:35:53 +0800

    Product css added.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,38 +3,71 @@ import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link, Navigate } from "react-router-dom";
 import ProductTimeline from "pages/Product";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    width: "133px",
-    height: "40px",
-    border: "none",
-    "border-radius": "5px",
-    textTransform: "none",
-    margin: theme.spacing(1),
-    backgroundColor: "#5048E5",
-    color: "#FFFFFF",
-    "&:hover": {
-      backgroundColor: "#512da8",
+    button: {
+        width: "133px",
+        height: "40px",
+        border: "none",
+        "border-radius": "5px",
+        textTransform: "none",
+        margin: theme.spacing(1),
+        backgroundColor: "#5048E5",
+        color: "#FFFFFF",
+        "&:hover": {
+            backgroundColor: "#512da8",
+        },
     },
-  },
 }));
 
 export default function Home() {
-  const classes = useStyles();
-  const { isAuthenticated } = useSelector((state) => state.user);
+    const classes = useStyles();
+    const { isAuthenticated } = useSelector((state) => state.user);
 
-  if (!isAuthenticated) {
-    return <Navigate to="/login" state={{ from: "/" }} />;
-  }
+    if (!isAuthenticated) {
+        return <Navigate to="/login" state={{ from: "/" }} />;
+    }
 
-  return (
-    <div>
-      <Link to="new-product">
-        <button className={classes.button}>Add Product</button>
-      </Link>
+    return (
+        <div>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    margin: "30px 20px 0 20px",
+                    alignItems: "baseline",
+                }}
+            >
+                <h1
+                    style={{
+                        fontWeight: "700",
+                        fontSize: "32px",
+                        flexGrow: "5",
+                    }}
+                >
+                    Products
+                </h1>
+                <TextField size="small" select defaultValue="last_added">
+                    <MenuItem key={"last_added"} value={"last_added"}>
+                        Last added
+                    </MenuItem>
+                    <MenuItem key={"low_to_high"} value={"low_to_high"}>
+                        Price: low to high
+                    </MenuItem>
+                    <MenuItem key={"high_to_low"} value={"high_to_low"}>
+                        Price: high to low
+                    </MenuItem>
+                </TextField>
+                <Link to="new-product">
+                    <button className={classes.button}>Add Product</button>
+                </Link>
+            </div>
 
-      <ProductTimeline />
-    </div>
-  );
+            {/* <div style={{ backgroundColor: "white", padding: "20px" }}> */}
+            <ProductTimeline />
+            {/* </div> */}
+        </div>
+    );
 }
--- client/src/pages/Product/index.js
+++ client/src/pages/Product/index.js
@@ -1,60 +1,121 @@
 import React, { useEffect, useState } from "react";
-import { List, Avatar, Skeleton, Space, Button, Typography, Modal } from "antd";
+import {
+    List,
+    Avatar,
+    Skeleton,
+    Space,
+    Button,
+    Typography,
+    Modal,
+    Card,
+} from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProductsAction } from "app/productSlice";
 import style from "./style.module.css";
 import { Link } from "react-router-dom";
+import { Padding } from "@mui/icons-material";
 
 export default function ProductList() {
-  const dispatch = useDispatch();
-  const { products, status } = useSelector((state) => state.products);
-  const { user } = useSelector((state) => state.user);
-  const [align] = useState("end");
-  //   console.log(user);
-  //   console.log(products);
+    const dispatch = useDispatch();
+    const { products, status } = useSelector((state) => state.products);
+    const { user } = useSelector((state) => state.user);
+    const [align] = useState("end");
+    const [position] = useState("bottom");
+    //   console.log(user);
+    //   console.log(products);
 
-  useEffect(() => {
-    dispatch(fetchProductsAction());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchProductsAction());
+    }, [dispatch]);
 
-  return (
-    <List
-      className={style.list}
-      loading={status === "pending"}
-      pagination={{
-        onChange: (page) => {
-          console.log(page);
-        },
-        pageSize: 3,
-        align,
-      }}
-      dataSource={products}
-      renderItem={(item, idx) => (
-        <List.Item
-          key={idx}
-          actions={[
-            // console.log(item.user),
-            //the userId and the item.user(product.userId) must exist
-            user.id && item.user && user.id === item.user._id
-              ? ((<Button>Add</Button>), (<Button>Edit</Button>))
-              : ((<Button>Add</Button>), (<Button disabled>Edit</Button>)),
-          ]}
-        >
-          <Skeleton title={false} loading={status === "pending"} active>
-            <List.Item.Meta
-              title={
-                <Link to={item._id}>
-                  <img width={100} alt="logo" src={item.link} />
-                </Link>
-              }
-              description={item.name}
-            />
-            <Typography.Paragraph style={{ fontSize: "1.1rem" }}>
-              ${item.price}
-            </Typography.Paragraph>
-          </Skeleton>
-        </List.Item>
-      )}
-    ></List>
-  );
+    return (
+        <List
+            grid={{
+                column: 5,
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 3,
+                lg: 4,
+                xl: 5,
+                xxl: 5,
+            }}
+            className={style.list}
+            loading={status === "pending"}
+            pagination={{
+                onChange: (page) => {
+                    console.log(page);
+                },
+                align,
+                position,
+            }}
+            dataSource={products}
+            renderItem={(item, idx) => (
+                <List.Item
+                    className={style.product_item}
+                    key={idx}
+                    // actions={[
+                    //     // console.log(item.user),
+                    //     //the userId and the item.user(product.userId) must exist
+                    //     user.id && item.user && user.id === item.user._id
+                    //         ? ((<Button>Add</Button>), (<Button>Edit</Button>))
+                    //         : ((<Button>Add</Button>),
+                    //           (<Button disabled>Edit</Button>)),
+                    // ]}
+                >
+                    <Skeleton
+                        title={false}
+                        loading={status === "pending"}
+                        active
+                    >
+                        <List.Item />
+                        <Card
+                            style={{
+                                border: "solid 1px #CCCCCC",
+                                borderRadius: "5px",
+                            }}
+                            bodyStyle={{ padding: "15px" }}
+                        >
+                            <Link to={item._id}>
+                                <div className={style.image_container}>
+                                    <img
+                                        className={style.image}
+                                        width={218}
+                                        style={{
+                                            maxWidth: "80%",
+                                            height: "auto",
+                                            margin: "auto",
+                                        }}
+                                        alt="logo"
+                                        src={item.link}
+                                    />
+                                </div>
+                                <h5 className={style.product_title}>
+                                    {item.name}
+                                </h5>
+                                <p
+                                    style={{
+                                        fontSize: "16px",
+                                        fontWeight: "600",
+                                        marginBottom: "4px",
+                                    }}
+                                >
+                                    ${item.price}
+                                </p>
+                            </Link>
+                            {[
+                                user.id &&
+                                item.user &&
+                                user.id === item.user._id
+                                    ? ((<Button>Add</Button>),
+                                      (<Button>Edit</Button>))
+                                    : ((<Button>Add</Button>),
+                                      (<Button disabled>Edit</Button>)),
+                            ]}
+                        </Card>
+                    </Skeleton>
+                </List.Item>
+            )}
+        ></List>
+    );
 }
